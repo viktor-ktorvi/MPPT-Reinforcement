6bb817b86966414a362de5bcfb611498b8eafe8b
Viktor <todosijevicviktor998@gmail.com>
2022-01-11 15:51:09 +0100

    Made it so that the MPP is found from a look up table

--- single panel/mppt/signle_panel_mppt.m
+++ single panel/mppt/signle_panel_mppt.m
@@ -9,11 +9,9 @@ set(groot, 'defaultLegendInterpreter','latex');
 sim_time = 1;   % simulation time
 n = 1;  % number of panels
 Voc = 43.99;
-ramp_slope = n * Voc / sim_time;  % n times Voc of one minus Vd
 Ts = 1e-3;  % sampling time
 max_time_step = 1e-4; % variable step solver max step size
 duty_step = 1/400;
-P_1000 = 175.087; % W
-P_500 = 86.79;
-%% Sim
-% out = sim('single_panel_mppt_sim.slx');
+
+% look up table data for mpp's of different irradiances at 25'C
+load('../power as a function of irradiance/mpp_versus_irr_data_at_T_25.mat')
--- single panel/power as a function of irradiance/power_and_irradiance.m
+++ single panel/power as a function of irradiance/power_and_irradiance.m
@@ -14,27 +14,29 @@ max_time_step = 1e-4; % variable step solver max step size
 Irradiance = 500; % W/m^2
 T = 25; % 'C
 %% Sim
-Irr = 50:50:1300;
-mppt_array = zeros(length(Irr), 1);
-for i = 1:length(Irr)
-    Irradiance = Irr(i);
+irr_data = 50:50:1300;
+mpp_data = zeros(length(irr_data), 1);
+for i = 1:length(irr_data)
+    Irradiance = irr_data(i);
     out = sim('../curve generation/single_panel_sim.slx');
     voltage = out.voltage.Data;
     current = out.current.Data;
     
     % 'MinPeakProminence' of p Wats
     [peaks, arg_peaks] = findpeaks(voltage .* current, 'MinPeakProminence', 1);
-    mppt_array(i) = max(peaks);
+    mpp_data(i) = max(peaks);
 end
+
+save("mpp_versus_irr_data_at_T_" + T, 'irr_data', 'mpp_data');
 %% Plot
 
-c = polyfit(Irr,mppt_array,1);
-line_est = polyval(c,Irr);
+c = polyfit(irr_data, mpp_data, 1);
+line_est = polyval(c, irr_data);
 
 figure
-plot(Irr, mppt_array, 'ro');
+plot(irr_data, mpp_data, 'ro');
 hold on;
-plot(Irr, line_est, 'b');
+plot(irr_data, line_est, 'b');
 title('Maximum power point as a function of irradiance')
 xlabel('Irradiance [$\frac{W}{m^2}$]')
 ylabel('MPP [$W$]')
